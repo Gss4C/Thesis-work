17da238bee04c8b0892ea204dbb25767a3f79a35
JonBon <jb5nnhy@gmail.com>
2023-03-09 11:01:46 +0100

    iniziato oggetti

--- full_selector.py
+++ full_selector.py
@@ -28,15 +28,22 @@ class full_selector(Module):
         electron   = Collection(event, "Electron")
         muons       = Collection(event, "Muon")
 
-        #filters jets and muons
+        #**********************#
+        #Collections
+        #**********************#
         goodJets_idx    = list(filter(lambda idx: jets[idx].pt     > 30  and  abs(jets[idx].eta)               < 2.4 and jets[idx].jetID >= 3, range(0, len(jets))))
         goodFjets_idx   = list(filter(lambda idx: fatjets[idx].pt  > 200 and  abs(fatjets[idx].eta)            < 2.4                         , range(0, len(fatjets))))
         goodEle_idx     = list(filter(lambda idx: electron[idx].pt > 30  and  electron[idx].cutBased_Fall17_V1 >= 2                          , range(0, len(electron))))
         goodMu_idx      = list(filter(lambda idx: muons[idx].pt    > 30  and  muons[idx].looseId                                             , range(0, len(muons))))
-
-        #salvo nel branch l'info dell'indice buono dell'oggetto nell'evento
+        #creating branches
         self.out.fillBranch("goodJet_idx",  goodJets_idx)
         self.out.fillBranch("goodFjet_idx", goodFjets_idx)
         self.out.fillBranch("goodEle_idx",  goodEle_idx)
         self.out.fillBranch("goodMu_idx",   goodMu_idx)
+
+        #**********************
+        #objects
+        #**********************
+        isGoodHLT = HLT.PFMETNoMu120_PFMHTNoMu120_IDTight and HLT.PFMET120_PFMHT120_IDTight
+        isGoodMET = MET.pt > 200
         
