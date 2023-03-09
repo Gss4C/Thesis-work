290695c58146be2288a45920de2f47bfc9d2a98f
JonBon <jb5nnhy@gmail.com>
2023-03-09 16:45:19 +0100

    defining global_veto

--- skimming_pre/boosted_resolved.py
+++ skimming_pre/boosted_resolved.py
@@ -12,19 +12,19 @@ class full_selector(Module):
         pass
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
-        self.out.branch("goodJets_idx", "F" , lenVar="ngoodJets")
-    def global_veto(self, MET, jets, muons, electrons):
+        self.out.branch("Boosted",  "0", lenVar="ngoodJets")
+        self.out.branch("Resolved", "0", lenVar="ngoodJets")
+
+    def global_veto(self, MET, deltaphi, electrons, muons):
         # non voglio ci siano leptoni
-    def 
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
-    def analyze(self,event):
+    def analyze(self,event): #qui raccolgo solo robaccia good
         HLT         = Object(event, "HLT")
         MET         = Object(event, "MET")
         jets        = Collection(event, "Jet")
-        fatjets     = Collection(event, "FatJet")
+        #fatjets     = Collection(event, "FatJet")
         electron    = Collection(event, "Electron")
         muons       = Collection(event, "Muon")
-
-        return goodEvent
+        
