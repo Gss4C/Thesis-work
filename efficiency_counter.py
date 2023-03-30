09e55d73b936334a1045b7c86214721b902696ad
JonBon <jb5nnhy@gmail.com>
2023-03-30 11:57:24 +0200

    Efficienze

--- efficiency_counter.py
+++ efficiency_counter.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 
 file_s = ROOT.TFile("/afs/cern.ch/user/j/jbonetti/CMSSW_10_5_0/src/PhysicsTools/NanoAODTools/crab/TprimeToTZM1800_Skim", "Open")
-getent = ROOT.TFile("EOS", "Open") #da aggiungere
+getent = ROOT.TFile("/eos/user/o/oiorio/samples/ttdm/TprimeToTZM1800.root'", "Open") 
 tree_s = file_s.Events
+tree_get = getent.Events
 #/home/iorio/public/tDM/tDM_mPhi1000_mChi1.root
 
 boost_w_fj  = 0
@@ -42,10 +43,16 @@ for event in range(tree_s.GetEntries()):
         else:
             res_wo_fj += 1
 
-print("boost with forward jets=      ", float(boost_w_fj)/getent.Events.GetEntries() )
-print("boost without forward jets=   ", float(boost_wo_fj)/getent.Events.GetEntries())
-print("resolved with forward jets=   ", float(res_w_fj)/getent.Events.GetEntries()   )
-print("resolved without forward jets=", float(res_wo_fj)/getent.Events.GetEntries()  )
+print("boost with forward jets=      ", float(boost_w_fj)/tree_get.Events.GetEntries() )
+print("boost without forward jets=   ", float(boost_wo_fj)/tree_get.Events.GetEntries())
+print("resolved with forward jets=   ", float(res_w_fj)/tree_get.Events.GetEntries()   )
+print("resolved without forward jets=", float(res_wo_fj)/tree_get.Events.GetEntries()  )
 
 epsilon = {}
 epsilon["boosted"]=[boost_w_fj*100, boost_wo_fj*100]
+epsilon["boosted"]=[res_w_fj*100, res_wo_fj*100]
+df = pd.DataFrame(data=epsilon , index=["fwd" , "not_fwd"])
+plt.rcParams["figure.figsize"]=[8, 5] 
+plt.rcParams["figure.autolayout"]=True 
+plt.title("Efficiencies (%)") 
+plot = sns.heatmap(epsilon, cmap="crest", annot=True)
