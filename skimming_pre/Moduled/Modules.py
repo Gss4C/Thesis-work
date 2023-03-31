cada3525048565ff386643c8d43f40471b2f9cc2
JonBon <jb5nnhy@gmail.com>
2023-03-31 12:17:30 +0200

    Creazione rimodulazione

---
+++ skimming_pre/Moduled/Modules.py
@@ -0,0 +1,5 @@
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection , Object
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+import ROOT
+import math
+ROOT.PyConfig.IgnoreCommandLineOptions = True 
