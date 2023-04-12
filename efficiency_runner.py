0a7d495c174cead97fa46dfa693c6a7e9b5d03be
JonBon <jb5nnhy@gmail.com>
2023-04-12 15:55:18 +0200

    import

--- efficiency_runner.py
+++ efficiency_runner.py
@@ -1,6 +1,6 @@
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 from PhysicsTools.NanoAODTools.postprocessing.jon_scr.mini_samples import *
-from PhysicsTools.NanoAODTools.postprocessing.jon_scr.efficiency_class import efficiency_make as efm
+from PhysicsTools.NanoAODTools.postprocessing.jon_scr.efficiency_class import classic_efficiency_make as efm
 import ROOT
 import seaborn as sns
 import matplotlib.pyplot as plt
