4d30a616ff996e733d2c89d31a60ca17fe615ed7
JonBon <jb5nnhy@gmail.com>
2023-04-12 15:52:36 +0200

    starting argparse

--- efficiency_runner.py
+++ efficiency_runner.py
@@ -5,6 +5,16 @@ import ROOT
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd #mi servono qua?
+import argparse
+
+parser = argparse.ArgumentParser(description='Description of your program')
+parser.add_argument('-c','--classic',
+                    help='Standard heatmap: (fwd, n_fwd) vs (boost_old, resolved)', 
+                    required=True)
+parser.add_argument('-f','--full', 
+                    help='Full heatmap: (fwd, n_fwd) vs (resolved, 4 * boost)', 
+                    required=True)
+args = vars(parser.parse_args())
 
 for dataset in dataset_list:
     efficiencyer = efm(dataset)
