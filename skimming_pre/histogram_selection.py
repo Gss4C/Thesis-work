5209811fa6e6f933027aad89d9caeaaf2c57edca
JonBon <jb5nnhy@gmail.com>
2023-04-21 16:26:17 +0200

    cose

--- skimming_pre/histogram_selection.py
+++ skimming_pre/histogram_selection.py
@@ -4,7 +4,15 @@ from PhysicsTools.NanoAODTools.postprocessing.Thesis.skimming_pre.eff_sig.effici
 
 percorso  = "/afs/cern.ch/user/j/jbonetti/CMSSW_10_5_0/src/PhysicsTools/NanoAODTools/crab/"
 
+variables=[("MET_pt"," MET "100,0,500)]
+cuts = [("boosted_deeptag")]
 for dataset in signal_only_list:
+    #mi piglio Tree->Events
     skim_dataset_file_name = percorso + dataset.name.replace(".root","_Skim.root")
     skimmed_file = ROOT.TFile(skim_dataset_file_name,"Open")
     skimmed_tree = skimmed_file.Events
+    var=variables[0]
+    cut=cuts[0]
+    houtput = ROOT.TH1F(var[0],var[0],var[1],var[2],var[3])
+    skimmed_tree.Project(houtput,var[0],cut[0])
+
