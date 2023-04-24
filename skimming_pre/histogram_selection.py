70cf91d85f008cdb50432e80c2d29010c52db143
JonBon <jb5nnhy@gmail.com>
2023-04-24 17:30:46 +0200

    Nuova funzione ancora da testare

--- skimming_pre/histogram_selection.py
+++ skimming_pre/histogram_selection.py
@@ -38,3 +38,41 @@ if options.type == 0:
             #n_mc = houtput.Integral()
             
             c.SaveAs("MET_signal_" + cut + dataset.name.replace(".root", "") + ".png")
+
+if options.type == 1:
+    bkg_histos = ROOT.TH1F()
+    for background in bkg_only_list:
+        for cut in cuts:
+            weights_histo_name = percorso + "hist_out_" + background.name
+            weights_histo_file = ROOT.TFile(weights_histo_name, 'Open')
+            weight_histo = weights_histo_file.plots.Get('h_genweight')
+            n_mc_tot = weight_histo.GetBinContent(1) 
+            w = background.sigma * L_run2 / (n_mc_tot)
+
+            skim_background_file_name = percorso + background.name.replace(".root","_Skim.root")
+            skimmed_file = ROOT.TFile(skim_background_file_name,"Open")
+            skimmed_tree = skimmed_file.Events
+
+            houtput = ROOT.TH1F('MET_pt','MET' + background.name ,100,0,1000)
+            skimmed_tree.Project(houtput.GetName(), 'MET_pt', cut)
+
+    for signal in signal_only_list:
+        for cut in cuts:
+            weights_histo_name = percorso + "hist_out_" + signal.name
+            weights_histo_file = ROOT.TFile(weights_histo_name, 'Open')
+            weight_histo = weights_histo_file.plots.Get('h_genweight')
+            n_mc_tot = weight_histo.GetBinContent(1) 
+            w = signal.sigma * L_run2 / (n_mc_tot)
+
+            skim_signal_file_name = percorso + signal.name.replace(".root","_Skim.root")
+            skimmed_file = ROOT.TFile(skim_signal_file_name,"Open")
+            skimmed_tree = skimmed_file.Events
+
+            c = ROOT.TCanvas()
+            houtput = ROOT.TH1F('MET_pt','MET' + signal.name ,100,0,1000)
+            skimmed_tree.Project(houtput.GetName(), 'MET_pt', cut)
+            
+            houtput.Scale(w)
+            c.Draw()
+            houtput.Draw()
+    
