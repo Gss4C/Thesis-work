b645ba598cd5aaa9846ce22eaab1da6a13ce1c0e
JonBon <jb5nnhy@gmail.com>
2023-04-24 17:31:15 +0200

    Aggiunta lista bkgonly

--- skimming_pre/mini_samples.py
+++ skimming_pre/mini_samples.py
@@ -79,6 +79,12 @@ signal_only_list = [TprimeBToTZ_M_600,
                     TprimeBToTZ_M_1100,
                     TprimeBToTZ_M_1800]
 
+bkg_only_list = [TT_Mtt_1000toInf,
+                 TT_Mtt_700to1000,
+                 ZJetsToNuNu_HT_2500ToInf,
+                 ZJetsToNuNu_HT_1200To2500,
+                 ZJetsToNuNu_HT_800To1200,
+                 ZJetsToNuNu_HT_600To800]
 signal_only_list_full = [TprimeBToTZ_M_600,
                          TprimeBToTZ_M_1000,
                          TprimeBToTZ_M_1100,
