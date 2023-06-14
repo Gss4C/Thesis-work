91990bfe8de7d37fcef7673d7c249edb79aadc2f
JonBon <jb5nnhy@gmail.com>
2023-06-14 12:20:06 +0200

    testing

--- threshold/th_evalutation.py
+++ threshold/th_evalutation.py
@@ -1,4 +1,4 @@
-from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+#from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 import ROOT
 from PhysicsTools.NanoAODTools.postprocessing.Thesis.meta_funcs.nonroot import *
 from PhysicsTools.NanoAODTools.postprocessing.Thesis.meta_funcs.histos import *
