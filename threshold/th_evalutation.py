e09afcccc889eb891862a1e067367de4da9ac505
JonBon <jb5nnhy@gmail.com>
2023-06-14 12:18:17 +0200

    testing

--- threshold/th_evalutation.py
+++ threshold/th_evalutation.py
@@ -2,6 +2,8 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collect
 import ROOT
 from PhysicsTools.NanoAODTools.postprocessing.Thesis.meta_funcs.nonroot import *
 from PhysicsTools.NanoAODTools.postprocessing.Thesis.meta_funcs.histos import *
-from PhysicsTools.NanoAODTools.postprocessing.Thesis.meta_funcs.histos import *
 
-#
+# ../../../../../../../src/PhysicsTools/NanoAODTools/crab/macros/files/
+
+lista = read_and_list("../../../../../../../src/PhysicsTools/NanoAODTools/crab/macros/files/TT_hadr_2018.txt")
+print(lista)
