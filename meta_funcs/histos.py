9ca05b9ec9a9d9343a2e91a4af22e2567f9ed517
JonBon <jb5nnhy@gmail.com>
2023-06-14 12:57:54 +0200

    implementing dict

--- meta_funcs/histos.py
+++ meta_funcs/histos.py
@@ -1,1 +1,13 @@
 import ROOT
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
+import awkward as ak
+
+#    Coffea histo maker for single file    #
+def histo_writer():
+single_tree = NanoEventsFactory.from_root(files_list[1], schemaclass=NanoAODSchema.v6).events()
+scores = ak.flatten(tree.TopLowPt[(tree.TopLowPt.truth==0)].scoreDNN)
+
+histo      = ROOT.TH1F(histoName, histoTitle, nbins, xmin, xmax)
+for score in scores:
+    histo.Fill(score)
+gisto.Write()
--- threshold/th_evalutation.py
+++ threshold/th_evalutation.py
@@ -1,8 +1,20 @@
-from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 import ROOT
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 from PhysicsTools.NanoAODTools.postprocessing.Thesis.meta_funcs.nonroot import *
 from PhysicsTools.NanoAODTools.postprocessing.Thesis.meta_funcs.histos import *
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 
-# ../../../../../../../src/PhysicsTools/NanoAODTools/crab/macros/files/
+txt_crab_files_path = "../../../../../../../src/PhysicsTools/NanoAODTools/crab/macros/files/"
+all_files_dict = {}
 
-lista = read_and_list("../../../../../../../src/PhysicsTools/NanoAODTools/crab/macros/files/TT_hadr_2018.txt")
+files_list = read_and_list("../../../../../../../src/PhysicsTools/NanoAODTools/crab/macros/files/TT_hadr_2018.txt")
+#le liste di file devo organizzarle in un dict di liste così ho tutto a portata di mano
+
+#apro il file, ne prendo uno a caso
+single_tree = NanoEventsFactory.from_root(files_list[1], schemaclass=NanoAODSchema.v6).events()
+scores = ak.flatten(tree.TopLowPt[(tree.TopLowPt.truth==0)].scoreDNN)
+
+histo      = ROOT.TH1F(histoName, histoTitle, nbins, xmin, xmax)
+for score in scores:
+    histo.Fill(score)
+histo.Write()
