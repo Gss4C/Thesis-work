951fcc76ec707d0ff2698ddb93bedf53cb86829e
JonBon <jb5nnhy@gmail.com>
2023-06-23 13:04:06 +0200

    debug nel caso di histo vuoti

--- threshold/thresholding_class.py
+++ threshold/thresholding_class.py
@@ -21,15 +21,35 @@ class threshold_evalutator:
 
 
     def threshold_F_seeker(self, bg_efficiency = 0.1):
-        full_integral = self.histo_false.Integral()
-        epsilon = 0
-        bin_idx = 0
-        while(epsilon < bg_efficiency and epsilon > 0):
-            bin_idx += 1
-            right_integral = self.histo_false.Integral(self.N_bins - bin_idx, self.N_bins)
-            epsilon = right_integral/full_integral if full_integral != 0 else 0
-        th_bin = self.N_bins - bin_idx
-        return th_bin, epsilon
+        full_integral = float(self.histo_false.Integral())
+        print("full integral: " + str(full_integral))
+        
+        epsilon = 0.001
+        th_bin  = 0
+
+        if full_integral < 1e-10: #se l'histo è vuoto deve fermarsi
+            print("Warning: Full integral is very close to zero. Cannot evalutate this threshold")
+            return th_bin, epsilon
+        else:        
+            bin_idx = 0
+            while epsilon < bg_efficiency and epsilon > 0:
+                bin_idx += 1
+                right_integral = self.histo_false.Integral(self.N_bins - bin_idx, self.N_bins)
+                
+                if right_integral > 1e-10 and full_integral > 1e-10:
+                    if full_integral != 0:
+                        epsilon = right_integral/full_integral
+                    else:
+                        epsilon = 0
+
+            th_bin = self.N_bins - bin_idx
+            if epsilon == 0:
+                print('^^^^    impossible to find threshold    ^^^^')
+                print("FAIL")
+            else:
+                print("^^^    Trovata la TH al bin: " + str(th_bin) + "   ^^^")
+                print("SUCCESS")
+            return th_bin, epsilon
 
     def threshold_binTodec(self, th_bin):
         unit = 1/(self.N_bins)
@@ -66,66 +86,58 @@ class thrashold_histomaker:
         
         
     def crea_4histo(self, batch_files_list, h_lowF, h_lowT, h_highF, h_highT, dataset_name,evalutate_threshold = True, N_bins = 250):
-        ############################
-        #    Filename Menagment    #
-        ############################
-        #filenames = self.json_reader(self.json_name)
-        #root_files = self.read_and_list(filenames["meta_info"]["parent_path"] + filenames["TT_2018"][1]) #qui va aggiunta la possibilità di selezionarsi la monnezza
-        tree  = NanoEventsFactory.from_root(batch_files_list[1], schemaclass=NanoAODSchema.v6).events()
-        #testing_dataset = filenames["TT_2018"][1].replace(".txt","") #nome file txt
+        for i in range(len(batch_files_list)):
+            if i%10 == 0:
+                completion_percentage = (i / len(batch_files_list)) * 100
+                print(f"Percentuale di completamento dataset: {completion_percentage:.2f}%")
+            
+            tree = NanoEventsFactory.from_root(batch_files_list[i], schemaclass=NanoAODSchema.v6).events()
+            #print(len(tree))
+            ########################
+            #    Histo Creation    #
+            ########################
+            if ak.any(tree.TopLowPt.scoreDNN):
+                scores_lowF  = ak.flatten(tree.TopLowPt[(tree.TopLowPt.truth==0)].scoreDNN)
+                scores_lowT  = ak.flatten(tree.TopLowPt[(tree.TopLowPt.truth==1)].scoreDNN)
+            else:
+                scores_lowF  = []
+                scores_lowT  = []
+            if ak.any(tree.TopHighPt.score2):
+                scores_highF = ak.flatten(tree.TopHighPt[(tree.TopHighPt.truth==0)].score2)
+                scores_highT = ak.flatten(tree.TopHighPt[(tree.TopHighPt.truth==1)].score2)
+            else:
+                scores_highF  = []
+                scores_highT  = []
 
-        ########################
-        #    Histo Creation    #
-        ########################
-        if ak.any(tree.TopLowPt.scoreDNN):
-            scores_lowF  = ak.flatten(tree.TopLowPt[(tree.TopLowPt.truth==0)].scoreDNN)
-            scores_lowT  = ak.flatten(tree.TopLowPt[(tree.TopLowPt.truth==1)].scoreDNN)
+            for score_lf in scores_lowF:
+                h_lowF.Fill(score_lf)
+            for score_lt in scores_lowT:
+                h_lowT.Fill(score_lt)
+            for score_hf in scores_highF:
+                h_highF.Fill(score_hf)
+            for score_ht in scores_highT:
+                h_highT.Fill(score_ht)
 
-        else:
-            scores_lowF  = []
-            scores_lowT  = []
-        if ak.any(tree.TopHighPt.score2):
-            scores_highF = ak.flatten(tree.TopHighPt[(tree.TopHighPt.truth==0)].score2)
-            scores_highT = ak.flatten(tree.TopHighPt[(tree.TopHighPt.truth==1)].score2)
-        else:
-            scores_highF  = []
-            scores_highT  = []
-        #print('DEBUG: Salto la creazione degli istogrammi, returno cose vuote a caso')
-        #print('print del comando ROOT.gDirectory.pwd()')
-        #print(ROOT.gDirectory.pwd())
-        '''
-        h_lowF  = ROOT.TH1F("Lowpt_False" + dataset_name ,"Lowpt_False" + dataset_name , N_bins, 0 ,1)
-        h_lowT  = ROOT.TH1F("Lowpt_True" + dataset_name  ,"Lowpt_True" + dataset_name  , N_bins, 0 ,1)
-        h_highF = ROOT.TH1F("Highpt_False" + dataset_name,"Highpt_False" + dataset_name, N_bins, 0 ,1)
-        h_highT = ROOT.TH1F("Highpt_True" + dataset_name ,"Highpt_True" + dataset_name , N_bins, 0 ,1)
-        '''
-        for score_lf, score_lt, score_hf, score_ht in zip(scores_lowF, scores_lowT, scores_highF, scores_highT):
-            if ak.any(scores_lowF):  h_lowF.Fill(score_lf)
-            if ak.any(scores_lowT):  h_lowT.Fill(score_lt)
-            if ak.any(scores_highF): h_highF.Fill(score_hf)
-            if ak.any(scores_highT): h_highT.Fill(score_ht)
-        
-        
         ########################
         #    Calcolo Soglie    #    
         ########################
+        print('Inizio calcolo soglie per il dataset...')
         if evalutate_threshold:
             LowEval = threshold_evalutator(N_bins      = N_bins,
-                                           histo_true  = h_lowT,
-                                           histo_false = h_lowF)
+                                            histo_true  = h_lowT,
+                                            histo_false = h_lowF)
             HighEval = threshold_evalutator(N_bins      = N_bins, 
                                             histo_true  = h_highT, 
                                             histo_false = h_highF)
-            print("istanziati gli oggetti")
 
             LowTh_Bin,  LowEpsilon  = LowEval.threshold_F_seeker()
-            print("trovate le threshold")
             LowTh = LowEval.threshold_binTodec(LowTh_Bin)
+            print("Soglia in decimale di Low: " + str(LowTh))
             h_lowF.SetBinContent(0,LowTh)
             h_lowT.SetBinContent(0,LowTh)
-            
 
             HighTh_Bin, HighEpsilon = HighEval.threshold_F_seeker()
             HighTh = HighEval.threshold_binTodec(HighTh_Bin)
+            print("Soglia in decimale di High: " + str(HighTh))
             h_highF.SetBinContent(0,HighTh)
             h_highT.SetBinContent(0,HighTh)
